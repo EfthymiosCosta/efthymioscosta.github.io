c53b02e285b9b574a4cc54fd4a2bb7aeb7d93701
Costa <efthymios.costa@gmail.com>
2025-01-29 14:10:07 +0000

    update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,7 @@
 var addressPoints = [
-  [
-    "San José, Costa Rica",
-    9.9281,
-    -84.0907
-  ],
-  [
-    "London, United Kingdom",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "Bologna, Italy",
-    44.49381,
-    11.33875
-  ],
-  [
-    "Berlin, Germany",
-    52.52437,
-    13.41053
-  ]
+  ["San José, Costa Rica", 9.9281, -84.0907],
+  ["London, United Kingdom", 51.5073219, -0.1276473],
+  ["Bologna, Italy", 44.49381, 11.33875],
+  ["Berlin, Germany", 52.52437, 13.41053],
+  ["Stresa, Italy", 45.8882, 8.5258]
 ];
