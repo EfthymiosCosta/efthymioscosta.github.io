5fa100059716374bb4a1fe49e5b9ab3ba7ffd1a0
Costa <efthymios.costa@gmail.com>
2025-05-23 09:52:46 +0100

    update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,5 +3,6 @@ var addressPoints = [
   ["London, United Kingdom", 51.5073219, -0.1276473],
   ["Bologna, Italy", 44.49381, 11.33875],
   ["Berlin, Germany", 52.52437, 13.41053],
-  ["Stresa, Italy", 45.8882, 8.5258]
+  ["Stresa, Italy", 45.8882, 8.5258],
+  ["Larnaca, Cyprus", 35.0083, 33.6968]
 ];
